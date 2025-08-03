41d3d24d4a9deb76912304f8670a096691fa6060
techsolutionss <aulematins@gmail.com>
2025-08-03 15:41:44 +0100

    changes

--- config/settings/production.py
+++ config/settings/production.py
@@ -8,7 +8,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 
 # ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["oharematerials.com"])
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["www.sqanalysis.com", "sqanalysis.com"]
 
 
 # DATABASES
--- sq_env/Lib/site-packages/user_visit/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserVisitAppConfig(AppConfig):
-    name = "user_visit"
-    verbose_name = "User visit log"
-    default_auto_field = "django.db.models.AutoField"
